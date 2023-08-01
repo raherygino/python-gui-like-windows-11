3b43eed321051f259f39fb546a0d6dbf208cb7dc
gino <georgino197@gmail.com>
2023-08-01 19:57:46 +0200

    New update

--- view/main/interfaces/__init__.py
+++
@@ -1,6 +0,0 @@
-from .home import HomeFrame
-from .settings import SettingsFrame
-from .products.add import AddProductFrame
-from .products.list import ListProductsFrame
-from .invoices.add import AddInvoiceFrame
-from .invoices.list import ListInvoicesFrame
