d4ad10c88617c123d30053e7b5cd71b20ed2df2b
gino <georgino197@gmail.com>
2023-08-01 19:49:44 +0200

    Gallery Interface init

---
+++ app/view/gallery_interface.py
@@ -0,0 +1,91 @@
+# coding:utf-8
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen, QColor
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from qfluentwidgets import (ScrollArea, isDarkTheme, Theme, TitleLabel, CaptionLabel)
+
+from ..common.config import cfg
+from ..common.style_sheet import StyleSheet
+from ..components.ExampleCard import ExampleCard
+from ..components.Card import Card
+
+class SeparatorWidget(QWidget):
+    """ Seperator widget """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setFixedSize(6, 16)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        pen = QPen(1)
+        pen.setCosmetic(True)
+        c = QColor(255, 255, 255, 21) if isDarkTheme() else QColor(0, 0, 0, 15)
+        pen.setColor(c)
+        painter.setPen(pen)
+
+        x = self.width() // 2
+        painter.drawLine(x, 0, x, self.height())
+
+
+class ToolBar(QWidget):
+    """ Tool bar """
+
+    def __init__(self, title, subtitle, parent=None):
+        super().__init__(parent=parent)
+        self.titleLabel = TitleLabel(title, self)
+        self.subtitleLabel = CaptionLabel(subtitle, self)
+        self.vBoxLayout = QVBoxLayout(self)
+
+        self.__initWidget()
+
+    def __initWidget(self):
+        self.setFixedHeight(90)
+        self.vBoxLayout.setSpacing(0)
+        self.vBoxLayout.setContentsMargins(36, 22, 36, 12)
+        self.vBoxLayout.addWidget(self.titleLabel)
+        self.vBoxLayout.addWidget(self.subtitleLabel)
+        self.vBoxLayout.setAlignment(Qt.AlignTop)
+
+    def toggleTheme(self):
+        theme = Theme.LIGHT if isDarkTheme() else Theme.DARK
+        cfg.set(cfg.themeMode, theme)
+
+class GalleryInterface(ScrollArea):
+    """ Gallery interface """
+
+    def __init__(self, title: str, subtitle: str, parent=None):
+        super().__init__(parent=parent)
+        self.view = QWidget(self)
+        self.toolBar = ToolBar(title, subtitle, self)
+        self.vBoxLayout = QVBoxLayout(self.view)
+
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setViewportMargins(0,70, 0, 0)
+        self.setWidget(self.view)
+        self.setWidgetResizable(True)
+
+        self.vBoxLayout.setSpacing(30)
+        self.vBoxLayout.setAlignment(Qt.AlignTop)
+        self.vBoxLayout.setContentsMargins(36, 20, 36, 36)
+
+        self.view.setObjectName('view')
+
+        StyleSheet.GALLERY_INTERFACE.apply(self)
+
+    def addExampleCard(self, title, widget, sourcePath: str, stretch=0):
+        card = ExampleCard(title, widget, sourcePath, stretch, self.view)
+        self.vBoxLayout.addWidget(card, 0, Qt.AlignTop)
+        return card
+        
+    def addCard(self, title, widget) -> Card:
+        return Card(title, widget, self.view, self.vBoxLayout)
+
+    def scrollToCard(self, index: int):
+        """ scroll to example card """
+        w = self.vBoxLayout.itemAt(index).widget()
+        self.verticalScrollBar().setValue(w.y())
+
+    def resizeEvent(self, e):
+        super().resizeEvent(e)
+        self.toolBar.resize(self.width(), self.toolBar.height())
