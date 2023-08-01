ccf824a7a4cbe82f6686390a8a1ba1d2eb451b3f
gino <georgino197@gmail.com>
2023-08-01 19:50:45 +0200

    New architecture

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+# coding:utf-8
+import os
 import sys
-from view import *
 
-def setApplication() -> QApplication:
-    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+from PyQt5.QtCore import Qt, QTranslator
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication
+from qfluentwidgets import FluentTranslator
+
+from app.common.config import cfg
+from app.view.main_window import MainWindow
+
+
+# enable dpi scale
+if cfg.get(cfg.dpiScale) == "Auto":
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
     QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-    return QApplication(sys.argv)
+else:
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = str(cfg.get(cfg.dpiScale))
 
-if __name__ == '__main__':
-    app = setApplication()
-    setTheme(Theme.DARK)
-    setThemeColor("#142170", False)
-    w = MainWindow()
-    w.show()
-    app.exec_()
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+# create application
+app = QApplication(sys.argv)
+app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
+
+# internationalization
+locale = cfg.get(cfg.language).value
+translator = FluentTranslator(locale)
+galleryTranslator = QTranslator()
+galleryTranslator.load(locale, "gallery", ".", ":/gallery/i18n")
+
+app.installTranslator(translator)
+app.installTranslator(galleryTranslator)
+
+# create main window
+w = MainWindow()
+w.show()
+
+app.exec_()
