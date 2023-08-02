1c3558074d01d6b18828b6aeaf27ccc33de01b5d
gino <georgino197@gmail.com>
2023-08-02 20:59:37 +0200

    Update Setting interface

---
+++ app/view/utils/setting_interface.py
@@ -0,0 +1,202 @@
+# coding:utf-8
+from qfluentwidgets import (SettingCardGroup, SwitchSettingCard, FolderListSettingCard,
+                            OptionsSettingCard, PushSettingCard,
+                            HyperlinkCard, PrimaryPushSettingCard, ScrollArea, ComboBox,
+                            ComboBoxSettingCard, ExpandLayout, Theme, CustomColorSettingCard,
+                            setTheme, setThemeColor, RangeSettingCard, isDarkTheme)
+from qfluentwidgets import FluentIcon as FIF
+from qfluentwidgets import InfoBar
+from PyQt5.QtCore import Qt, pyqtSignal, QUrl, QStandardPaths
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtWidgets import QWidget, QLabel, QFileDialog
+
+from ...common.icon import Icon
+from ...common.config import cfg, HELP_URL, FEEDBACK_URL, AUTHOR, VERSION, YEAR
+from ...common.style_sheet import StyleSheet
+from ...components.layout.AppCard import AppCard
+
+
+class SettingInterface(ScrollArea):
+    """ Setting interface """
+
+    checkUpdateSig = pyqtSignal()
+    musicFoldersChanged = pyqtSignal(list)
+    acrylicEnableChanged = pyqtSignal(bool)
+    downloadFolderChanged = pyqtSignal(str)
+    minimizeToTrayChanged = pyqtSignal(bool)
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.scrollWidget = QWidget()
+        self.expandLayout = ExpandLayout(self.scrollWidget)
+
+        # setting label
+        self.settingLabel = QLabel(self.tr("Settings"), self)
+
+        # personalization
+        self.personalGroup = SettingCardGroup(
+            self.tr('Personalization'), self.scrollWidget)
+        self.themeCard = OptionsSettingCard(
+            cfg.themeMode,
+            FIF.BRUSH,
+            self.tr('Application theme'),
+            self.tr("Change the appearance of your application"),
+            texts=[
+                self.tr('Light'), self.tr('Dark'),
+                self.tr('Use system setting')
+            ],
+            parent=self.personalGroup
+        )
+        self.themeColorCard = CustomColorSettingCard(
+            cfg.themeColor,
+            FIF.PALETTE,
+            self.tr('Theme color'),
+            self.tr('Change the theme color of you application'),
+            self.personalGroup
+        )
+        self.zoomCard = OptionsSettingCard(
+            cfg.dpiScale,
+            FIF.ZOOM,
+            self.tr("Interface zoom"),
+            self.tr("Change the size of widgets and fonts"),
+            texts=[
+                "100%", "125%", "150%",
+                self.tr("Use system setting")
+            ],
+            parent=self.personalGroup
+        )
+        self.comboBox = ComboBox(self)
+
+        items = ['English', 'French']
+        self.comboBox.addItems(items)
+        self.comboBox.setCurrentIndex(0)
+        #self.comboBox.currentTextChanged.connect(print)
+        self.comboBox.move(200, 200)
+        #self.languageCard = AppCard(FIF.LANGUAGE, 'Language', 'Set your preferred language for UI', self.comboBox)
+
+
+        self.languageCard = PrimaryPushSettingCard(
+            None,
+            FIF.LANGUAGE,
+            'Language',
+            'Set your preferred language for UI',
+            self.personalGroup
+        )
+
+
+        self.languageCard.hBoxLayout.removeWidget(self.languageCard.button)
+        self.languageCard.button = None;
+        self.languageCard.hBoxLayout.addWidget(self.comboBox)
+        self.languageCard.hBoxLayout.setContentsMargins(14,0,14,0)
+        # update software
+        self.updateSoftwareGroup = SettingCardGroup(
+            self.tr("Software update"), self.scrollWidget)
+        self.updateOnStartUpCard = SwitchSettingCard(
+            FIF.UPDATE,
+            self.tr('Check for updates when the application starts'),
+            self.tr('The new version will be more stable and have more features'),
+            configItem=cfg.checkUpdateAtStartUp,
+            parent=self.updateSoftwareGroup
+        )
+
+        # application
+        self.aboutGroup = SettingCardGroup(self.tr('About'), self.scrollWidget)
+        self.helpCard = HyperlinkCard(
+            HELP_URL,
+            self.tr('Open help page'),
+            FIF.HELP,
+            self.tr('Help'),
+            self.tr(
+                'Discover new features and learn useful tips about PyQt-Fluent-Widgets'),
+            self.aboutGroup
+        )
+        self.feedbackCard = PrimaryPushSettingCard(
+            self.tr('Provide feedback'),
+            FIF.FEEDBACK,
+            self.tr('Provide feedback'),
+            self.tr('Help us improve PyQt-Fluent-Widgets by providing feedback'),
+            self.aboutGroup
+        )
+        
+       # print()
+        self.aboutCard = PrimaryPushSettingCard(
+            self.tr('Check update'),
+            FIF.INFO,
+            self.tr('About'),
+            '© ' + self.tr('Copyright') + f" {YEAR}, {AUTHOR}. " +
+            self.tr('Version') + " " + VERSION,
+            self.aboutGroup
+        )
+
+        self.__initWidget()
+
+    def __initWidget(self):
+        self.resize(1000, 800)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setViewportMargins(0, 80, 0, 20)
+        self.setWidget(self.scrollWidget)
+        self.setWidgetResizable(True)
+        self.setObjectName('settingInterface')
+
+        # initialize style sheet
+        self.scrollWidget.setObjectName('scrollWidget')
+        self.settingLabel.setObjectName('settingLabel')
+        StyleSheet.SETTING_INTERFACE.apply(self)
+
+        # initialize layout
+        self.__initLayout()
+        self.__connectSignalToSlot()
+
+    def __initLayout(self):
+        self.settingLabel.move(36, 30)
+
+        self.personalGroup.addSettingCard(self.themeCard)
+        self.personalGroup.addSettingCard(self.themeColorCard)
+        self.personalGroup.addSettingCard(self.zoomCard)
+        self.personalGroup.addSettingCard(self.languageCard)
+
+
+        self.updateSoftwareGroup.addSettingCard(self.updateOnStartUpCard)
+
+        self.aboutGroup.addSettingCard(self.helpCard)
+        self.aboutGroup.addSettingCard(self.feedbackCard)
+        self.aboutGroup.addSettingCard(self.aboutCard)
+
+        # add setting card group to layout
+        self.expandLayout.setSpacing(28)
+        self.expandLayout.setContentsMargins(36, 10, 36, 0)
+        self.expandLayout.addWidget(self.personalGroup)
+        self.expandLayout.addWidget(self.updateSoftwareGroup)
+        self.expandLayout.addWidget(self.aboutGroup)
+
+    def __showRestartTooltip(self):
+        """ show restart tooltip """
+        InfoBar.success(
+            self.tr('Updated successfully'),
+            self.tr('Configuration takes effect after restart'),
+            duration=1500,
+            parent=self
+        )
+
+    def __onDownloadFolderCardClicked(self):
+        """ download folder card clicked slot """
+        folder = QFileDialog.getExistingDirectory(
+            self, self.tr("Choose folder"), "./")
+        if not folder or cfg.get(cfg.downloadFolder) == folder:
+            return
+
+        cfg.set(cfg.downloadFolder, folder)
+        self.downloadFolderCard.setContent(folder)
+
+    def __connectSignalToSlot(self):
+        """ connect signal to slot """
+        cfg.appRestartSig.connect(self.__showRestartTooltip)
+        cfg.themeChanged.connect(setTheme)
+
+        # personalization
+        self.themeColorCard.colorChanged.connect(setThemeColor)
+
+        # about
+        self.aboutCard.clicked.connect(self.checkUpdateSig)
+        self.feedbackCard.clicked.connect(
+            lambda: QDesktopServices.openUrl(QUrl(FEEDBACK_URL)))
